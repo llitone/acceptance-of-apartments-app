086e0ecc2324234db056c7c9bda3a65cb0c58d0e
D1ffic00lt <dm.filinov@gmail.com>
2023-03-31 17:58:56 +0700

    api example

---
+++ api/report_generator.py
@@ -0,0 +1,23 @@
+class ReportGenerator(object):
+    def __init__(self, json):
+        pass
+
+json_example = {
+    "category_of_problem_name":
+        [
+            {
+                "name": "",
+                "place": "",
+                "description": "",
+                "images":
+                    {
+                        "filename": "",
+                        "metadata": b""
+                    }
+            }
+        ]
+}
+
+# import requests
+#
+# print(requests.get("http://127.0.0.1:5000/app/api/v1.0/generator_status").json())
