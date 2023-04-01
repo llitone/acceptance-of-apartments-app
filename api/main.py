aa428fbdc7696ae7b437e58a3b7837ced26aad80
D1ffic00lt <dm.filinov@gmail.com>
2023-04-01 12:30:16 +0700

    api post create

--- api/main.py
+++ api/main.py
@@ -1,14 +1,16 @@
-# import requests
 from flask import Flask, jsonify, make_response, request, abort
 from flask_httpauth import HTTPBasicAuth
 
-auth = HTTPBasicAuth()
+from api.db import Database
+
 api = Flask(__name__)
+auth = HTTPBasicAuth()
+db = Database("server.db")
 
 @auth.get_password
 def get_password(username):
-    if username == "":
-        return username
+    if username == "shu_sha":
+        return "antony_car"
     return None
 
 @auth.error_handler
@@ -21,8 +23,18 @@ def get_detection(detection_id):
     # TODO: response from db
     if not detection_id and request.json:
         abort(404)
-    return make_response(jsonify(...), 201)
+    return make_response(jsonify({"jopa": 123}), 201)
 
+@api.route("/app/api/v1.0/db/detections", methods=["POST"])
+@auth.login_required
+def post_detection():
+    if not request.json:
+        abort(400)
+    try:
+        db.insert(request.json)
+    except Exception as ex:
+        return {"success": False, "error": str(ex)}, 400
+    return {"success": True}, 201
 
 @api.errorhandler(404)
 def not_found(error):
--- api/report_generator.py
+++ api/report_generator.py
@@ -1,23 +1,53 @@
+import base64
+
 class ReportGenerator(object):
     def __init__(self, json):
-        pass
+        self.json = json
 
 json_example = {
-    "category_of_problem_name":
+    1337: {
+        "Личная жизнь":
         [
             {
-                "name": "",
-                "place": "",
-                "description": "",
+                "name": "Московкин Александр",
+                "place": "it-квадрат",
+                "description": "МОСКВА ЖЕНАТ НА АЛЁНЕ И НЕ МОЖЕТ ЖЕНИТЬСЯ НА АНТОНЕ",
                 "images":
-                    {
-                        "filename": "",
-                        "metadata": b""
-                    }
+                    [
+                        {
+                            "filename": "IMG_4162.HEIC",
+                            "metadata": base64.b64encode(open("db/IMG_4162.HEIC", "rb").read()).decode("utf-8")
+                        },
+                        {
+                            "filename": "IMG_4149.heic",
+                            "metadata": base64.b64encode(open("db/IMG_4149.heic", "rb").read()).decode("utf-8")
+                        }
+                    ]
+            },
+            {
+                "name": "Личная жизнь антона петлина",
+                "place": "весь мир",
+                "description": "её нет",
+                "images": []
+            }
+        ],
+        "fuck": [
+            {
+                "name": "Физика",
+                "place": "школа",
+                "description": "у максона 4...",
+                "images": []
             }
         ]
+    }
 }
 
-# import requests
-#
-# print(requests.get("http://127.0.0.1:5000/app/api/v1.0/generator_status").json())
+import requests.auth
+
+print(
+    requests.post(
+        "http://127.0.0.1:5000/app/api/v1.0/db/detections",
+        json=json_example,
+        auth=requests.auth.HTTPBasicAuth("shu_sha", "antony_car")
+    ).json()
+)
